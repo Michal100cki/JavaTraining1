b695b3fb6975324b67e9f885240d018e96d544ae
Michal Stocki <micheal.stocki@gmail.com>
2017-07-12 15:12:49 +0200

    pierwsza aktualizacja Array20170712

--- Array20170712/src/Glowna.java
+++ Array20170712/src/Glowna.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 /**
  * Created by Rokers on 2017-07-12.
  */
 public class Glowna {
+    private static Scanner scanner =new Scanner(System.in);
+
     public static void main(String[] args){
 System.out.println("Hallo");
+       int zmienna=0;
+
+
+        zmienna=scanner.nextInt();
+        System.out.println("My name is ..."+zmienna);
     }
 
 
